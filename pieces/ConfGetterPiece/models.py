fc572b47ca6c56d3360ae684092e90b04162b876
Wiley <wileywu@163.com>
2025-09-24 09:18:30 +0800

    new version

--- pieces/ConfGetterPiece/models.py
+++ pieces/ConfGetterPiece/models.py
@@ -1,13 +1,75 @@
-from pydantic import BaseModel, Field
-from typing import List, Dict, Any
+from pydantic import BaseModel, Field, ConfigDict
+from domino.models import OutputModifierModel, OutputModifierItemType
+from typing import List, Union
+from datetime import date as dt_date, datetime as dt_datetime, time as dt_time
+
+
+class InputKwargsModel(BaseModel):
+    kwarg_name: str = Field(
+        default=None,
+        description='Argument name.',
+        json_schema_extra={
+            "from_upstream": "never"
+        }
+    )
+    kwarg_value: Union[str, list, int, float, bool, dict, dt_date, dt_time, dt_datetime] = Field(
+        default=None,
+        description='Argument value.',
+        json_schema_extra={
+            "from_upstream": "always"
+        }
+    )
+
 
 class InputModel(BaseModel):
-    keys: List[str] = Field(
-        description="需要提取的 dag_run.conf 中的 key 列表",
-        example=["region", "bucket", "start_date", "end_date"]
+    """
+    ParamBuilder Input Model
+    """
+#     input_args: List[InputKwargsModel] = Field(
+#         default=[
+#             InputKwargsModel(kwarg_value="", kwarg_name="kwarg_2"),
+#             InputKwargsModel(kwarg_value="", kwarg_name="kwarg_1"),
+#         ],
+#         description='Input arguments.',
+#         json_schema_extra={
+#             "from_upstream": "never"
+#         }
+#     )
+#     script: str = Field(
+#         default="""# Do not modify the function definition line
+# def custom_function(kwarg_1, kwarg_2):
+#     # Write your code here
+#     print(f"First argument: {kwarg_1}")
+#     print(f"Second argument: {kwarg_2}")
+
+#     # Return the output of the function as an object,
+#     # Matching the Output Args defined in the Form below
+#     return {
+#         "output_1": "this is a string",
+#         "output_2": 420
+#     }
+# """,
+#         description='Python script.',
+#         json_schema_extra={
+#             "from_upstream": "never",
+#             'widget': "codeeditor-python",
+#         }
+#     )
+    output_args: List[OutputModifierModel] = Field(
+        default=[
+            OutputModifierModel(name="output_1", type=OutputModifierItemType.string, description="An example string output"),
+            OutputModifierModel(name="output_2", type=OutputModifierItemType.integer, description="An example integer output"),
+        ],
+        description='Output arguments.',
+        json_schema_extra={
+            "from_upstream": "never"
+        }
     )
 
+
 class OutputModel(BaseModel):
-    conf_dict: Dict[str, Any] = Field(
-        description="按 key 提取后的 conf 值字典"
-    )
+    """
+    ParamBuilder Output Model
+    """
+    # ref: https://docs.pydantic.dev/latest/concepts/models/#extra-fields
+    model_config = ConfigDict(extra='allow')
--- pieces/ConfGetterPiece/piece.py
+++ pieces/ConfGetterPiece/piece.py
@@ -4,9 +4,21 @@ from airflow.models import TaskInstance
 from typing import Dict, Any
 
 class ConfGetterPiece(BasePiece):
-    def piece_function(self, input_data: InputModel) -> OutputModel:
-        ti: TaskInstance = self.context["ti"]
-        conf: Dict[str, Any] = ti.dag_run.conf or {}
-        extracted = {k: conf.get(k) for k in input_data.keys}
-        self.logger.info("提取 conf 成功: %s", extracted)
-        return OutputModel(conf_dict=extracted)
+    def piece_function(self, input_data: InputModel):
+        # ti: TaskInstance = self.context["ti"]
+        # conf: Dict[str, Any] = ti.dag_run.conf or {}
+        # extracted = {k: conf.get(k) for k in input_data.keys}
+        # self.logger.info("提取 conf 成功: %s", extracted)
+        # return OutputModel(conf_dict=extracted)
+        # Log inputs
+        self.logger.info(f"""
+        Input Output args:\n{input_data.output_args}\n
+        """)
+        
+        output_args: Dict[str, Any] = {"output_1":"para1", "output_2":"para2"}
+
+        # Log output
+        self.logger.info(output_args)
+
+        # Return output
+        return OutputModel(**output_args)
