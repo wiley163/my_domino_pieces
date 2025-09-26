d38cb81489c6045aa5d2ff6868a4269e3eae5c8b
Wiley <wileywu@163.com>
2025-09-26 11:51:03 +0800

    StringifyPiece3

--- pieces/StringifyPiece/piece.py
+++ pieces/StringifyPiece/piece.py
@@ -6,9 +6,8 @@ class StringifyPiece(BasePiece):
 
     def piece_function(self, input_data: InputModel):
 
-        # string_value = str(input_data.input_value)
-        string_value = f"items: {input_data.input_value}\n"
-        self.logger.info(f"Input2 value as string: {string_value}")
+        string_value = str(input_data.input_value)
+        self.logger.info(f"Input23 value as string: {string_value}")
 
         # Return output
         return OutputModel(
