2c59bc1ec677aa68139ff93e428f1a28558cf541
Wiley <wileywu@163.com>
2025-09-28 14:42:04 +0800

    031

--- pieces/ConfGetterPiece/piece.py
+++ pieces/ConfGetterPiece/piece.py
@@ -3,21 +3,29 @@ from .models import InputModel, OutputModel
 from typing import Dict, Any
 
 class ConfGetterPiece(BasePiece):
-    def piece_function(self, input_data: InputModel):
+    def piece_function(self, input_data: InputModel, **kwargs):
         # ti: TaskInstance = self.context["ti"]
         # conf: Dict[str, Any] = ti.dag_run.conf or {}
         # extracted = {k: conf.get(k) for k in input_data.keys}
         # self.logger.info("提取 conf 成功: %s", extracted)
         # return OutputModel(conf_dict=extracted)
         # Log inputs
+        # 1. 从kwargs中安全地获取dag_run对象
+        dag_run = kwargs.get('dag_run')
+        
+        # 2. 从dag_run对象中获取conf字典
+        # 最好进行一次检查，以防dag_run不存在（虽然在正常运行中它总是存在）
+        dag_run_conf = {}
+        if dag_run:
+            dag_run_conf = dag_run.conf or {} # 如果conf为None，则使用空字典
         self.logger.info(f"""
-        self.context:\n{self.context}\n
+        dag_run_conf===>:\n{dag_run_conf}\n
         """)
         self.logger.info(f"""
         Input Output args:\n{input_data.output_args}\n
         """)
-        
-        output_args: Dict[str, Any] = {"output_1":"para111111", "output_2":"para222222"}
+        extracted = {k.name: dag_run_conf.get(k.name) for k in input_data.output_args}
+        output_args: Dict[str, Any] = extracted
 
         # Log output
         self.logger.info(output_args)
